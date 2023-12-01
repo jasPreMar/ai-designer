d6f58154290eae2011526f16e8e3eab4d5c464d7
jasPreMar <jasonpmarsh@gmail.com>
2023-11-30 18:00:34 -0600

    added node stuff

--- api/generateLayout.js
+++ api/generateLayout.js
@@ -1,25 +1,22 @@
-const fetch = require('node-fetch');
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 module.exports = async (req, res) => {
     const { prompt } = req.body;
     const systemPrompt = `Create an HTML layout based on this prompt: ${prompt}`;
 
     try {
-        const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                prompt: systemPrompt,
-                max_tokens: 150
-            })
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: systemPrompt,
+            max_tokens: 150,
         });
 
-        const { choices } = await response.json();
-        const generatedHtml = choices[0].text.trim();
-
+        const generatedHtml = response.data.choices[0].text.trim();
         res.status(200).json({ generatedHtml });
     } catch (error) {
         console.error('Error:', error);
